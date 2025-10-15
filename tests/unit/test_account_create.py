8f2e7cd6f9a09aa199ebf6a68aac467dc7b67f3f
wpalkaa <poukoxy@gmail.com>
2025-10-15 15:16:48 +0200

    F1+F2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,9 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678912")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.pesel == "12345678912"
+        assert len(account.pesel) == 11, 'TESTEST' 
+        assert account.balance == 0
