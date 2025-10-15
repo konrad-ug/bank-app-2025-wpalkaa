e2025df3f6d1456b8eeeee40ab1cb9b825fc2683
wpalkaa <poukoxy@gmail.com>
2025-10-15 15:38:43 +0200

    F3_v2.0

--- src/account.py
+++ src/account.py
@@ -2,5 +2,17 @@ class Account:
     def __init__(self, first_name, last_name, pesel):
         self.first_name = first_name
         self.last_name = last_name
-        self.pesel = pesel
-        self.balance = 0
+        self.balance = 0.0
+        
+        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        # if self.is_pesel_valid(pesel):
+        #     self.pesel = pesel
+        # else:
+        #     self.pesel = "Invalid"
+        
+        self.promo_code = None
+        
+    def is_pesel_valid(self, pesel):
+        if pesel and len(pesel) == 11:
+            return True
+        return False
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -7,8 +7,19 @@ class TestAccount:
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.pesel == "12345678912"
-        # assert len(account.pesel) == 11, 'TESTEST' 
-        if len(account.pesel) != 11:
-            account.pesel = "Invalid"
-            raise AssertionError("Invalid pesel length")
+        # if len(account.pesel) != 11:
+        #     account.pesel = "Invalid"
+        #     raise AssertionError("Invalid pesel length")
         assert account.balance == 0
+        
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Smith", "12345")    
+        assert account.pesel == "Invalid"
+
+    def test_pesel_too_long(self):
+        account = Account("Aclie", "Johnson", "12345")
+        assert account.pesel == "Invalid"
+        
+    def test_pesel_too_empty(self):
+        account = Account("Aclie", "Johnson", "")
+        assert account.pesel == "Invalid"
